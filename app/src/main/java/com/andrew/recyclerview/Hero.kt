4877393d4eea8da8c065c68961c06e5f7b29d51b
aNdr3W03 <andrewbjamesie@yahoo.com>
2022-07-22 20:17:11 +0700

    Initial commit

---
+++ app/src/main/java/com/andrew/recyclerview/Hero.kt
@@ -0,0 +1,7 @@
+package com.andrew.recyclerview
+
+data class Hero (
+    var name: String = "",
+    var detail: String = "",
+    var photo: Int = 0
+)
---
+++ app/src/main/java/com/andrew/recyclerview/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.andrew.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvHeroes: RecyclerView
+    private var list: ArrayList<Hero> = arrayListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(HeroesData.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+
+            }
+
+            R.id.action_grid -> {
+
+            }
+
+            R.id.action_cardview -> {
+
+            }
+        }
+    }
+}
